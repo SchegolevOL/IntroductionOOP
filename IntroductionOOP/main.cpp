26a614fcaaa8d2adc6de5b5294495bd1820c3808
SchegolevOL <92328776+SchegolevOL@users.noreply.github.com>
2021-11-26 16:35:08 +0500

    Operators

--- IntroductionOOP/main.cpp
+++ IntroductionOOP/main.cpp
@@ -11,7 +11,10 @@ using std::endl;
 #define tab "\t"
 //#define STRUCT_POINT
 //#define CONSRTUCTOR CHECK
+//#define DISTANCE
 
+
+#define ASSIGNMENT_CHECK
 class Point
 {
 	double x;
@@ -53,13 +56,37 @@ public:
 		this->y = double();
 		cout << "1argConstructor:\t" << this << endl;
 	}*/
-
+	Point(const Point& other)// конструктор копирования
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor" << this << endl;
+	}
+	
 	
 
 	~Point()
 	{
 		cout << "Destructor:\t" << this << endl;
 	}
+
+
+//				Operators
+
+	/*void operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment :\t" << this << endl;
+	}*/
+	Point& operator=(Point other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment :\t" << this << endl;
+		return *this;
+	}
+
 
 	//				Methods:
 	void print()const
@@ -94,13 +121,10 @@ int main()
 	cout << pA->x << tab << pA->y << endl;
 #endif // STRUCT_POINT
 #ifdef CONSRTUCTOR CHECK
-
-
-
-	Point A(3,-6);
-	/*A.set_x(2);
-	A.set_y(3);*/
-	//cout << A.get_x() << tab << A.get_y() << endl;
+	Point A(3,5);
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << tab << A.get_y() << endl;
 	A.print();
 	Point B(4, -5);
 	B.print();
@@ -109,13 +133,34 @@ int main()
 	Point D;
 	D.print();
 	
-	cout << A.distance(B.get_x(), B.get_y()) << endl;
-	cout << distance(A.get_x(), A.get_y(), B.get_x(), B.get_y()) << endl;
+	Point E = A;
+	Point F(A);
+
+	E.print();
+	Point G;
+	G = E;
+
+	
 #endif // CONSRTUCTOR CHECK
+#ifdef DISTANCE
 	Point A(2, 5);
 	Point B(3, 6);
 	cout << "Растояние от точки А до точки В : " << B.distance(A) << endl;
 	cout << "Растояние между точкой B и точкой A : " << distance(B, A) << endl;
+#endif // DISTANCE
+
+#ifdef ASSIGNMENT_CHECK
+		int a, b, c;
+	a = b = c = 0;
+	cout << a << tab << b << tab << c << endl;
+
+	Point A, B, C;
+	A = B = C = Point(2, 3);
+	A.print();
+	B.print();
+	C.print();
+#endif // ASSIGNMENT_CHECK
+
 
 	return 0;
 }
