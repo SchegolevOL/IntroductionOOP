91b8255fe2e32193aaec1140513316a7860fe6d5
SchegolevOL <92328776+SchegolevOL@users.noreply.github.com>
2021-12-20 22:53:36 +0500

    Delegetion dan

--- ClassString/main.cpp
+++ ClassString/main.cpp
@@ -36,30 +36,34 @@ public:
 	}
 
 	//-----------Constructors---------------------
-	explicit String(int size = 80)
+	explicit String(int size = 80) :
+		size(size),
+		str(new char[size] {})
 	{
-		this->size = size;
-		this->str = new char[size] {};//Память обезательно нужно занулить
+		/*this->size = size;
+		this->str = new char[size] {};*///Память обезательно нужно занулить
 		cout << "SizeConstructor: " << this << endl;
 	}
-	String(const char str[])
+	String(const char str[]): 
+		String(strlen(str) + 1)
 	{
-		this->size = strlen(str) + 1;
-		this->str = new char[size] {};
-		for (size_t i = 0; i < size; i++)
+		/*this->size = strlen(str) + 1;
+		this->str = new char[size] {};*/
+		for (size_t i = 0; str[i]; i++)
 		{
 			this->str[i] = str[i];
 		}
 		cout << "Constructor: " << this << endl;
 	}
-	String(const String& other)
+	String(const String& other):
+		String(other.str)
 	{
-		this->size = other.size;
+		/*this->size = other.size;
 		this->str = new char[size];
 		for (size_t i = 0; i < size; i++)
 		{
 			this->str[i] = other.str[i];
-		}
+		}*/
 		cout << "CopyConstructor: " << this << endl;
 	}
 
@@ -117,7 +121,8 @@ public:
 
 
 //#define CONSTRACTOR_CHEK
-//#define OPERATORS_CHEK
+#define OPERATORS_CHEK
+//#define INPUT_CHECK
 int main()
 {
 	SetConsoleCP(1251);
@@ -138,21 +143,22 @@ int main()
 #ifdef OPERATORS_CHEK
 	String str1 = "Heloo";
 	String str2 = "World";
+
 	String str3 = str1 + str2;
 	cout << str3 << endl;
 	cin >> str1;
 	cout << str1 << endl;
-	str2 += str1;;
+	str2 += str1;
 	cout << str2 << endl;
 #endif // OPERATORS_CHEK
-
+#ifdef INPUT_CHECK
 	String str;
 	cout << "Введите строку : ";
 	//cin >> str;
 	str.print();
 	getline(cin, str);
 	cout << str << endl;
-
+#endif // INPUT_CHECK
 
 
 
