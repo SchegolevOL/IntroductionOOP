92653bc152c9e08e4e35bb4966b41dc50df05401
SchegolevOL <92328776+SchegolevOL@users.noreply.github.com>
2021-12-22 23:49:02 +0500

    del eror

--- ClassMatrix/main.cpp
+++ ClassMatrix/main.cpp
@@ -22,13 +22,13 @@ int main()
 	matrix1.rnd(a, b);
 	cout << "Матрица №1"<<endl;
 	matrix1.print();
-	cout << "Определитель матрицы №1 = " << determinant(matrix1) << endl;
+	//cout << "Определитель матрицы №1 = " << determinant(matrix1) << endl;
 	cout << "\n\n";
 	Matrix matrix2(row, colum);
 	matrix2.rnd(a, b);
 	cout << "Матрица №2" << endl;
 	matrix2.print();
-	cout << "Определитель матрицы №2 = " << determinant(matrix2) << endl;
+	//cout << "Определитель матрицы №2 = " << determinant(matrix2) << endl;
 
 	cout << "\n\n";
 	Matrix matrix3;
@@ -36,63 +36,85 @@ int main()
 	try
 	{
 		matrix3 = matrix1 + matrix2;
+		matrix3.print();
 	}
 	catch (const std::exception& e)
 	{
 		cerr << e.what() << endl;
 	}
-	matrix3.print();
+	
 
 	cout << "Матрица №3 = Матрица №1 - Матрица №2" << endl;
 	try
 	{
 		matrix3 = matrix1 - matrix2;
+		matrix3.print();
 	}
 	catch (const std::exception& e)
 	{
 		cerr << e.what() << endl;
 	}
-	matrix3.print();
+	
 
 	cout << "Матрица №3 = Матрица №1 - Матрица №2" << endl;
 	try
 	{
 		matrix3 = matrix1 - matrix2;
+		matrix3.print();
 	}
 	catch (const std::exception& e)
 	{
 		cerr << e.what() << endl;
 	}
-	matrix3.print();
+	
 
 	cout << "Матрица №3 = Матрица №1 * Матрица №2" << endl;
 	try
 	{
 		matrix3 = matrix1 * matrix2;
+		matrix3.print();
 	}
 	catch (const std::exception& e)
 	{
 		cerr << e.what() << endl;
 	}
-	matrix3.print();
+	
 
 	cout << "Матрица №3 = Матрица №1 / Матрица №2" << endl;
 	try
 	{
 		matrix3 = matrix1 / matrix2;
+		matrix3.print();
 	}
 	catch (const std::exception& e)
 	{
 		cerr << e.what() << endl;
 	}
-	matrix3.print();
+	
 
 	cout << "Обратная матрица Матрицы №1 = " << endl;
+	try
+	{
 	matrix3 = Inverse(matrix1);
 	matrix3.print();
+	}
+	catch (const std::exception& e)
+	{
+		cerr << e.what() << endl;
+	}
+
+	
 	cout << "Проверка обратной матрицы" << endl;
+	try
+	{
 	matrix2 = matrix1 * matrix3;
 	matrix2.print();
+	}
+	catch (const std::exception& e)
+	{
+		cerr << e.what() << endl;
+	}
+	
 
 	return 0;
 }
